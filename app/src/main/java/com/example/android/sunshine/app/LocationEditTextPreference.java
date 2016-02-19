cfcd7105bcb3e60ba8e7bccfb445b912eb8a4091
Ricky Martinez <crosscountry68@gmail.com>
2016-02-19 15:56:25 -0800

    finished  8.03

--- app/src/main/java/com/example/android/sunshine/app/LocationEditTextPreference.java
+++ app/src/main/java/com/example/android/sunshine/app/LocationEditTextPreference.java
@@ -74,7 +74,7 @@ public class LocationEditTextPreference extends EditTextPreference {
 
                 // Launch the Place Picker so that the user can specify their location, and then
                 // return the result to SettingsActivity.
-                // TODO(student): Create a PlacePicker.IntentBuilder object here.
+                PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
 
 
                 // We are in a view right now, not an activity. So we need to get ourselves
@@ -82,13 +82,12 @@ public class LocationEditTextPreference extends EditTextPreference {
                 // SettingsActivity in this way, we can ensure the result of the Place Picker
                 // intent comes to the right place for us to process it.
                 Activity settingsActivity = (SettingsActivity) context;
-                //try {
-                    // TODO(student): Launch the intent using your settingsActivity object to access
-                    // startActivityForResult(). You'll need to build your builder object and use
-                    // the request code we declared in SettingsActivity.
+                try {
+                    settingsActivity.startActivityForResult(
+                            builder.build(settingsActivity), SettingsActivity.PLACE_PICKER_REQUEST);
 
-                //} catch (GooglePlayServicesNotAvailableException
-                 //       | GooglePlayServicesRepairableException e) {
+                } catch (GooglePlayServicesNotAvailableException
+                        | GooglePlayServicesRepairableException e) {
                     // What did you do?? This is why we check Google Play services in onResume!!!
                     // The difference in these exception types is the difference between pausing
                     // for a moment to prompt the user to update/install/enable Play services vs
@@ -97,7 +96,7 @@ public class LocationEditTextPreference extends EditTextPreference {
                     // by responding to these exceptions in the right moment. But I prefer a cleaner
                     // user experience, which is why you check all of this when the app resumes,
                     // and then disable/enable features based on that availability.
-                //}
+                }
             }
         });
 
