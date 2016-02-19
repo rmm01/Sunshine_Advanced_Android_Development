fd98b90414a9a1f9af61e6577d02326a0c4a73fc
Ricky Martinez <crosscountry68@gmail.com>
2016-02-19 15:37:47 -0800

    finished lesson  8.1 starting lesson 8.02

--- app/src/main/java/com/example/android/sunshine/app/LocationEditTextPreference.java
+++ app/src/main/java/com/example/android/sunshine/app/LocationEditTextPreference.java
@@ -15,17 +15,26 @@
  */
 package com.example.android.sunshine.app;
 
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.preference.EditTextPreference;
-import android.support.v7.app.AlertDialog;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.ui.PlacePicker;
 
 public class LocationEditTextPreference extends EditTextPreference {
     static final private int DEFAULT_MINIMUM_LOCATION_LENGTH = 2;
@@ -42,8 +51,58 @@ public class LocationEditTextPreference extends EditTextPreference {
         } finally {
             a.recycle();
         }
+
+        // Check to see if Google Play services is available. The Place Picker API is available
+        // through Google Play services, so if this is false, we'll just carry on as though this
+        // feature does not exist. If it is true, however, we can add a widget to our preference.
+        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+        int resultCode = apiAvailability.isGooglePlayServicesAvailable(getContext());
+        if (resultCode == ConnectionResult.SUCCESS) {
+            // Add the get current location widget to our location preference
+            setWidgetLayoutResource(R.layout.pref_current_location);
+        }
     }
 
+    @Override
+    protected View onCreateView(ViewGroup parent) {
+        View view = super.onCreateView(parent);
+        View currentLocation = view.findViewById(R.id.current_location);
+        currentLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = getContext();
+
+                // Launch the Place Picker so that the user can specify their location, and then
+                // return the result to SettingsActivity.
+                // TODO(student): Create a PlacePicker.IntentBuilder object here.
+
+
+                // We are in a view right now, not an activity. So we need to get ourselves
+                // an activity that we can use to start our Place Picker intent. By using
+                // SettingsActivity in this way, we can ensure the result of the Place Picker
+                // intent comes to the right place for us to process it.
+                Activity settingsActivity = (SettingsActivity) context;
+                //try {
+                    // TODO(student): Launch the intent using your settingsActivity object to access
+                    // startActivityForResult(). You'll need to build your builder object and use
+                    // the request code we declared in SettingsActivity.
+
+                //} catch (GooglePlayServicesNotAvailableException
+                 //       | GooglePlayServicesRepairableException e) {
+                    // What did you do?? This is why we check Google Play services in onResume!!!
+                    // The difference in these exception types is the difference between pausing
+                    // for a moment to prompt the user to update/install/enable Play services vs
+                    // complete and utter failure.
+                    // If you prefer to manage Google Play services dynamically, then you can do so
+                    // by responding to these exceptions in the right moment. But I prefer a cleaner
+                    // user experience, which is why you check all of this when the app resumes,
+                    // and then disable/enable features based on that availability.
+                //}
+            }
+        });
+
+        return view;
+    }
 
     @Override
     protected void showDialog(Bundle state) {
@@ -69,11 +128,16 @@ public class LocationEditTextPreference extends EditTextPreference {
                 if (d instanceof AlertDialog) {
                     AlertDialog dialog = (AlertDialog) d;
                     Button positiveButton = dialog.getButton(AlertDialog.BUTTON_POSITIVE);
-                    positiveButton.setEnabled(s.length() >= mMinLength);
+                    // Check if the EditText is empty
+                    if (s.length() < mMinLength) {
+                        // Disable OK button
+                        positiveButton.setEnabled(false);
+                    } else {
+                        // Re-enable the button.
+                        positiveButton.setEnabled(true);
+                    }
                 }
             }
-
-
         });
     }
 }
